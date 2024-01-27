319485a8cd71f3dc978046c329e8687f413f1ff8
ilfarpro <ilfarpro@protonmail.com>
2024-01-27 15:53:14 +0300

    remove maxrate because it crushes quality, use only crf instead

--- src/main.ts
+++ src/main.ts
@@ -7,20 +7,7 @@ let transcodingManager : PluginTranscodingManager
 
 const DEFAULT_HARDWARE_DECODE : boolean = false
 const DEFAULT_QUALITY : number = 6
-const DEFAULT_CRF : number = 28
 const DEFAULT_GOP : number = 2
-const DEFAULT_BITRATES : Map<VideoResolution, number> = new Map([
-    [VideoResolution.H_NOVIDEO, 64 * 1000],
-    [VideoResolution.H_144P, 700 * 1000],
-    [VideoResolution.H_240P, 1500 * 1000],
-    [VideoResolution.H_360P, 2600 * 1000],
-    [VideoResolution.H_480P, 4200 * 1000],
-    [VideoResolution.H_720P, 7000 * 1000],
-    [VideoResolution.H_1080P, 13000 * 1000],
-    [VideoResolution.H_1440P, 19_000 * 1000],
-    [VideoResolution.H_4K, 36_000 * 1000]
-])
-
 const DEFAULT_CRF_RES : Map<VideoResolution, number> = new Map([
     [VideoResolution.H_NOVIDEO, 28],
     [VideoResolution.H_144P, 28],
@@ -37,18 +24,14 @@ const DEFAULT_CRF_RES : Map<VideoResolution, number> = new Map([
 interface PluginSettings {
     hardwareDecode : boolean
     quality: number
-    crf: number
     gop: number
-    baseBitrate: Map<VideoResolution, number>
-    crf_per_res: Map<VideoResolution, number>
+    crfPerResolution: Map<VideoResolution, number>
 }
 let pluginSettings : PluginSettings = {
     hardwareDecode: DEFAULT_HARDWARE_DECODE,
     quality: DEFAULT_QUALITY,
-    crf: DEFAULT_CRF,
     gop: DEFAULT_GOP,
-    baseBitrate: new Map(DEFAULT_BITRATES),
-    crf_per_res: new Map(DEFAULT_CRF_RES)
+    crfPerResolution: new Map(DEFAULT_CRF_RES)
 }
 
 let latestStreamNum = 9999
@@ -113,46 +96,6 @@ export async function register({settingsManager, peertubeHelpers, transcodingMan
     })
 
     registerSetting({
-        name: 'crf',
-        label: 'CRF',
-
-        type: 'select',
-        options: [
-            { label: 'Recommended (28)', value: '28' },
-            { label: '38', value: '38' },
-            { label: '37', value: '37' },
-            { label: '36', value: '36' },
-            { label: '35', value: '35' },
-            { label: '34', value: '34' },
-            { label: '33', value: '33' },
-            { label: '32', value: '32' },
-            { label: '31', value: '31' },
-            { label: '30', value: '30' },
-            { label: '29', value: '29' },
-            { label: '28', value: '28' },
-            { label: '27', value: '27' },
-            { label: '26', value: '26' },
-            { label: '25', value: '25' },
-            { label: '24', value: '24' },
-            { label: '23', value: '23' },
-            { label: '22', value: '22' },
-            { label: '21', value: '21' },
-            { label: '20', value: '20' },
-            { label: '19', value: '19' },
-            { label: '18', value: '18' },
-            { label: '17', value: '17' },
-            { label: '16', value: '16' },
-            { label: '15', value: '15' },
-            { label: '14', value: '14' },
-        ],
-
-        descriptionHTML: 'This parameter controls the compression / quality tradeoff. Lower values mean better quality but higher filesize and higher bandwidth consumption. You may need to experiment to find the best value for you.',
-
-        default: DEFAULT_CRF.toString(),
-        private: false
-    })
-
-    registerSetting({
         name: 'gop',
         label: 'GoP size (seconds)',
 
@@ -172,31 +115,6 @@ export async function register({settingsManager, peertubeHelpers, transcodingMan
     })
 
     registerSetting({
-        name: 'base-bitrate-description',
-        label: 'Max bitrate',
-
-        type: 'html',
-        html: '',
-        descriptionHTML: `The base bitrate for video in bits. Plugin prefers minimum bitrate between the bitrate setting and video bitrate.<br/>This bitrate used when the video is transcoded at 30 FPS. The bitrate will be scaled linearly between this value and the maximum bitrate when the video is transcoded at 60 FPS. Wrong values are replaced by default values.`,
-           
-        private: true,
-    })
-    for (const [resolution, bitrate] of pluginSettings.baseBitrate) {
-        logger.info("registering bitrate setting: "+ bitrate.toString())
-        registerSetting({
-            name: `base-bitrate-${resolution}`,
-            label: `Max bitrate for ${printResolution(resolution)}`,
-
-            type: 'input',
-
-            default: DEFAULT_BITRATES.get(resolution)?.toString(),
-            descriptionHTML: `Default value: ${DEFAULT_BITRATES.get(resolution)}`,
-
-            private: false
-        })
-    }
-
-    registerSetting({
         name: 'crf-res-description',
         label: 'CRF per Resolution',
 
@@ -206,8 +124,8 @@ export async function register({settingsManager, peertubeHelpers, transcodingMan
            
         private: true,
     })
-    for (const [resolution, crf_per_res] of pluginSettings.crf_per_res) {
-        logger.info("registering crf setting: "+ crf_per_res.toString())
+    for (const [resolution, crfPerResolution] of pluginSettings.crfPerResolution) {
+        logger.info("registering crf setting: "+ crfPerResolution.toString())
         registerSetting({
             name: `crf-for-${resolution}`,
             label: `CRF for ${printResolution(resolution)}`,
@@ -235,26 +153,17 @@ export async function unregister() {
 async function loadSettings(settingsManager: PluginSettingsManager) {
     pluginSettings.hardwareDecode = await settingsManager.getSetting('hardware-decode') == "true"
     pluginSettings.quality = parseInt(await settingsManager.getSetting('quality') as string) || DEFAULT_QUALITY
-    pluginSettings.crf = parseInt(await settingsManager.getSetting('crf') as string) || DEFAULT_CRF
     pluginSettings.gop = parseInt(await settingsManager.getSetting('gop') as string) || DEFAULT_GOP
 
-    for (const [resolution, bitrate] of DEFAULT_BITRATES) {
-        const key = `base-bitrate-${resolution}`
-        const storedValue = await settingsManager.getSetting(key) as string
-        pluginSettings.baseBitrate.set(resolution, parseInt(storedValue) || bitrate)
-        logger.info(`Bitrate ${printResolution(resolution)}: ${pluginSettings.baseBitrate.get(resolution)}`)
-    }
-
-    for (const [resolution, crf_per_res] of DEFAULT_CRF_RES) {
+    for (const [resolution, crfPerResolution] of DEFAULT_CRF_RES) {
         const key = `crf-for-${resolution}`
         const storedValue = await settingsManager.getSetting(key) as string
-        pluginSettings.crf_per_res.set(resolution, parseInt(storedValue) || crf_per_res)
-        logger.info(`CRF for ${printResolution(resolution)}: ${pluginSettings.crf_per_res.get(resolution)}`)
+        pluginSettings.crfPerResolution.set(resolution, parseInt(storedValue) || crfPerResolution)
+        logger.info(`CRF for ${printResolution(resolution)}: ${pluginSettings.crfPerResolution.get(resolution)}`)
     }
 
     logger.info(`Hardware decode: ${pluginSettings.hardwareDecode}`)
     logger.info(`SVT-AV1 preset: ${pluginSettings.quality}`)
-    logger.info(`CRF: ${pluginSettings.crf}`)
     logger.info(`GOP: ${pluginSettings.gop}`)
 }
 
@@ -290,15 +199,10 @@ function buildInitOptions() {
 }
 
 async function vodBuilder(params: EncoderOptionsBuilderParams) : Promise<EncoderOptions> {
-    const { resolution, fps, streamNum, inputBitrate } = params
+    const { resolution, fps, streamNum } = params
     //const streamSuffix = streamNum == undefined ? '' : `:${streamNum}`
-    let targetBitrate = getTargetBitrate(resolution, fps)
-    let targetCRF = pluginSettings.crf_per_res.get(resolution) || 0
+    let targetCRF = pluginSettings.crfPerResolution.get(resolution) || 0
     let shouldInitVaapi = (streamNum == undefined || streamNum <= latestStreamNum)
-
-    if (targetBitrate > inputBitrate) {
-        targetBitrate = inputBitrate
-    }
 
     logger.info(`Building encoder options, received ${JSON.stringify(params)}`)
     
@@ -317,8 +221,6 @@ async function vodBuilder(params: EncoderOptionsBuilderParams) : Promise<Encoder
             `-preset ${pluginSettings.quality}`,
             `-pix_fmt yuv420p`,
             `-crf ${targetCRF}`,
-            `-maxrate ${targetBitrate}`,
-            `-bufsize ${targetBitrate * 2}`,
             `-g ${fps}*${pluginSettings.gop}`,
             `-svtav1-params tune=0:fast-decode=1:tile-rows=3:tile-columns=4:variance-boost-strength=2`
         ]
@@ -345,14 +247,9 @@ async function vodAudioBuilder(params: EncoderOptionsBuilderParams) : Promise<En
 }
 
 async function liveBuilder(params: EncoderOptionsBuilderParams) : Promise<EncoderOptions> {
-    const { resolution, fps, streamNum, inputBitrate } = params
+    const { fps, streamNum } = params
     const streamSuffix = streamNum == undefined ? '' : `:${streamNum}`
-    let targetBitrate = getTargetBitrate(resolution, fps)
     let shouldInitVaapi = (streamNum == undefined || streamNum <= latestStreamNum)
-
-    if (targetBitrate > inputBitrate) {
-        targetBitrate = inputBitrate
-    }
 
     logger.info(`Building encoder options, received ${JSON.stringify(params)}`)
 
@@ -372,28 +269,9 @@ async function liveBuilder(params: EncoderOptionsBuilderParams) : Promise<Encode
         `-profile:v${streamSuffix} high`,
         `-level:v${streamSuffix} 3.1`,
         `-g:v${streamSuffix} ${fps*1}`,
-        `-b:v${streamSuffix} ${targetBitrate}`,
-        `-bufsize ${targetBitrate * 2}`,
         `-map_metadata -1`,
       ]
     }
     logger.info(`EncoderOptions: ${JSON.stringify(options)}`)
     return options
-}
-
-/**
- * Calculate the target bitrate based on video resolution and FPS.
- *
- * The calculation is based on two values:
- * Bitrate at VideoTranscodingFPS.AVERAGE is always the same as
- * getBaseBitrate(). Bitrate at VideoTranscodingFPS.MAX is always
- * getBaseBitrate() * 1.6. All other values are calculated linearly
- * between these two points.
- */
-function getTargetBitrate (resolution : VideoResolution, fps : number) : number {
-    const baseBitrate = pluginSettings.baseBitrate.get(resolution) || 0
-    const maxBitrate = baseBitrate * 1.6
-    const maxBitrateDifference = maxBitrate - baseBitrate
-    const maxFpsDifference = 60 - 30
-    return Math.floor(baseBitrate + (fps - 30) * (maxBitrateDifference / maxFpsDifference))
 }
